62d2d6b5e4104ed35c1fb4122ed251a4cbbdaeae
dmitrsavk <dmitrsavk@yandex.ru>
2017-10-29 14:48:07 +0300

    add input moment

--- src/components/MainPage/Form/Form.jsx
+++ src/components/MainPage/Form/Form.jsx
@@ -4,15 +4,59 @@ import {
 	FormGroup,
 	ControlLabel,
 	FormControl,
-	Button
+	Button,
+	Form
 } from 'react-bootstrap';
 
+import moment from 'moment';
+import 'moment/locale/ru';
+import InputMoment from 'input-moment';
+import 'input-moment/dist/input-moment.css'
 import './Form.css';
 
-class Form extends Component {
+class OrderForm extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			loading: false,
+			openFirstDate: false,
+			openSecondDate: false,
+			firstDate: moment().locale('ru'),
+			secondDate: moment().locale('ru')
+		}
+
+		this.handleSecondDateChange = this.handleSecondDateChange.bind(this);
+		this.handleFirstDateChange = this.handleFirstDateChange.bind(this);
+		this.handleSecondDateSave = this.handleSecondDateSave.bind(this);
+		this.handleFirstDateSave = this.handleFirstDateSave.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSecondDateChange(moment) {
+		this.setState({secondDate: moment});
+	}
+
+	handleFirstDateChange(moment) {
+		this.setState({firstDate: moment});
+	}
+
+	handleSecondDateSave(moment) {
+		this.setState({openSecondDate: !this.state.openSecondDate});
+	}
+
+	handleFirstDateSave(moment) {
+		this.setState({openFirstDate: !this.state.openFirstDate});
+	}
+
+	handleSubmit(event) {
+        event.preventDefault();
+        console.log('submit')
+    }
+
 	render() {
 		return (
-			<form className='form'>
+			<Form className='form' onSubmit={this.handleSubmit}>
 				<div className='form__section'>
 					<div className='form__label'>Откуда</div>
 
@@ -38,23 +82,30 @@ class Form extends Component {
 					</FormGroup>
 					<FormGroup
 						controlId='time'
-						className='form__time'
-					>
-						<ControlLabel>Время</ControlLabel>
-						<FormControl
-							type='text'
-							placeholder='14:00'
-						/>
-					</FormGroup>
-					<FormGroup
-						controlId='time'
 						className='form__date'
 					>
 						<ControlLabel>Дата</ControlLabel>
 						<FormControl
 							type='text'
 							placeholder='Вторник, 12 ноября'
+							onClick={() => this.setState({
+								openFirstDate: !this.state.openFirstDate,
+								openSecondDate: false
+							})}
+							value={this.state.firstDate.format('lll')}
+							onChange={() => {}}
 						/>
+						<div className='form__date-wrap'>
+							{this.state.openFirstDate ?
+								<InputMoment
+									moment={this.state.firstDate}
+									onChange={this.handleFirstDateChange}
+									minStep={10}
+		            				onSave={this.handleFirstDateSave}
+								/> :
+								null
+							}
+						</div>
 					</FormGroup>
 				</div>
 
@@ -83,23 +134,30 @@ class Form extends Component {
 					</FormGroup>
 					<FormGroup
 						controlId='time'
-						className='form__time'
-					>
-						<ControlLabel>Время</ControlLabel>
-						<FormControl
-							type='text'
-							placeholder='14:00'
-						/>
-					</FormGroup>
-					<FormGroup
-						controlId='time'
 						className='form__date'
 					>
 						<ControlLabel>Дата</ControlLabel>
 						<FormControl
 							type='text'
 							placeholder='Вторник, 12 ноября'
+							onClick={() => this.setState({
+								openSecondDate: !this.state.openSecondDate,
+								openFirstDate: false
+							})}
+							value={this.state.secondDate.format('lll')}
+							onChange={() => {}}
 						/>
+						<div className='form__date-wrap'>
+							{this.state.openSecondDate ?
+								<InputMoment
+									moment={this.state.secondDate}
+									onChange={this.handleSecondDateChange}
+									minStep={10}
+		            				onSave={this.handleSecondDateSave}
+								/> :
+								null
+							}
+						</div>
 					</FormGroup>
 				</div>
 
@@ -113,7 +171,7 @@ class Form extends Component {
 					>
 						<ControlLabel>Ваш email</ControlLabel>
 						<FormControl
-							type='text'
+							type='email'
 							placeholder='support@citydeliver.ru'
 						/>
 					</FormGroup>
@@ -128,12 +186,12 @@ class Form extends Component {
 						/>
 					</FormGroup>
 					<div className='form__button-wrap'>
-						<Button>Отправить заявку</Button>
+						<Button type='submit'>Отправить заявку</Button>
 					</div>
 				</div>
-			</form>
+			</Form>
 		);
 	}
 }
 
-export default Form;
+export default OrderForm;
